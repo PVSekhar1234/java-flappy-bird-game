4760a7543f732ff2505ffe06c51c09a7fef2ddb1
PVSekhar1234 <cse210001051@iiti.ac.in>
2024-05-09 17:49:52 +0530

    adds gameloop and y-axis motion to bird by utilizing keyboard event

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
 
-public class FlappyBird extends JPanel{
+public class FlappyBird extends JPanel implements ActionListener, KeyListener {
     int boardWidth = 360;
     int boardHeight = 640;
 
@@ -35,10 +35,16 @@ public class FlappyBird extends JPanel{
 
     //game logic
     Bird bird;
+    int velocityY = 0; //move bird up/down speed.
+    int gravity = 1;
+
+    Timer gameLoop;
 
     FlappyBird() {
         setPreferredSize(new Dimension(boardWidth, boardHeight)); // for setting the size of the panel
-        setBackground(Color.blue); // for setting the background color of the panel
+        // setBackground(Color.blue); // for setting the background color of the panel
+        setFocusable(true);
+        addKeyListener(this);
 
         //load images
         backgroundImg = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
@@ -48,7 +54,10 @@ public class FlappyBird extends JPanel{
 
         //bird
         bird = new Bird(birdImg);
-
+        
+        //game timer
+		gameLoop = new Timer(1000/60, this); //how long it takes to start timer, milliseconds gone between frames 
+        gameLoop.start();
     }
 
     public void paintComponent(Graphics g) {
@@ -59,8 +68,35 @@ public class FlappyBird extends JPanel{
 	public void draw(Graphics g) {
         //background
         g.drawImage(backgroundImg, 0, 0, this.boardWidth, this.boardHeight, null);
-        
+
         //bird
         g.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height, null);
     }
+
+    public void move() {
+        //bird
+        velocityY += gravity;
+        bird.y += velocityY;
+        bird.y = Math.max(bird.y, 0); //apply gravity to current bird.y, limit the bird.y to top of the canvas
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) { //called every x milliseconds by gameLoop timer
+        move();
+        repaint();
+    }  
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            velocityY = -9;
+        }
+    }
+
+    //not needed
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    @Override
+    public void keyReleased(KeyEvent e) {}
 }
