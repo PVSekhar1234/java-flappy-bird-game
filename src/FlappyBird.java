a7efbdba2fb998b523a11ec3abae6003437feea0
PVSekhar1234 <cse210001051@iiti.ac.in>
2024-05-09 17:29:45 +0530

    adds loads all images and adds background

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -8,8 +8,31 @@ public class FlappyBird extends JPanel{
     int boardWidth = 360;
     int boardHeight = 640;
 
+    //images
+    Image backgroundImg;
+    Image birdImg;
+    Image topPipeImg;
+    Image bottomPipeImg;
+
     FlappyBird() {
         setPreferredSize(new Dimension(boardWidth, boardHeight)); // for setting the size of the panel
         setBackground(Color.blue); // for setting the background color of the panel
+
+        //load images
+        backgroundImg = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+        birdImg = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
+        topPipeImg = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
+        bottomPipeImg = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+
+    }
+
+    public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		draw(g);
+	}
+
+	public void draw(Graphics g) {
+        //background
+        g.drawImage(backgroundImg, 0, 0, this.boardWidth, this.boardHeight, null);
     }
 }
